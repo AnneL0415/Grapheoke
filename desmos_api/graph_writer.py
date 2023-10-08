d6b73e3712b9dba894584cbdd8b6e31ffffb9f29
Anne Lee <annelee0415@gmail.com>
2023-10-07 22:25:51 -0400

    plus music function

--- desmos_api/graph_writer.py
+++ desmos_api/graph_writer.py
@@ -55,13 +55,25 @@ def graph_fn(arr):
     # end lyric graph
     output_file.write("</script>\n\n")
 
-    # ------
+def music_fn(arr):
+    # create file
+    output_file = open("graph.html", "w")
 
-    # setup music graph
-    output_file.write("<div id=\"calculator\" style=\"width: 100%; height: 50%;\"></div>\n")
+    # set basic parameters
+    output_file.write("<script src=\"calculator.js\"></script>\n")
+    output_file.write("<div id=\"calculator\" style=\"width: auto; height: auto;\"></div>\n")
     output_file.write("<script>\n")
-    output_file.write("\tvar music_elt = document.getElementById('calculator');\n")
-    output_file.write("\tvar music_calculator = Desmos.GraphingCalculator(music_elt);\n\n")
+    output_file.write("\tvar elt = document.getElementById('calculator');\n")
+    output_file.write("\tvar calculator = Desmos.GraphingCalculator(elt);\n")
 
-    # end music graph
+    # write and format each function
+    i = 1
+    for fn in arr:
+        output_file.write("\tcalculator.setExpression({ id: 'line" + str(i) + "', latex: ")
+        output_file.write(repr(fn))
+        output_file.write(", color: '#000000' });\n")
+        i+=1
+    
+    # end file
+    output_file.write("\tcalculator.openKeypad();\n")
     output_file.write("</script>")
